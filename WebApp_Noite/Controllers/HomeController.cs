72531f79792a39027e4205648a3e08b462c66d96
GUILHERME SOUZA <guilherme003souza@gmail.com>
2023-03-24 21:44:33 -0300

    Estudando MVC C#

--- WebApp_Noite/Controllers/HomeController.cs
+++ WebApp_Noite/Controllers/HomeController.cs
@@ -18,6 +18,16 @@ namespace WebApp_Noite.Controllers
             return View();
         }
 
+        public IActionResult MinhaTela()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
