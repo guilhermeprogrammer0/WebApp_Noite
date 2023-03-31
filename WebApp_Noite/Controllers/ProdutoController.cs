0746f355739cd27baa0e539d5f8b5be88d07237f
GUILHERME SOUZA <guilherme003souza@gmail.com>
2023-03-31 20:09:33 -0300

    Criando Controllers

---
+++ WebApp_Noite/Controllers/ProdutoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Noite.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Listar()
+        {
+            return View();
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
