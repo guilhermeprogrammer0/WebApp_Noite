f02331b35067cb97da7a6bffea8063046b302321
GUILHERME SOUZA <guilherme003souza@gmail.com>
2023-03-31 21:36:56 -0300

    Criando controller Cliente

---
+++ WebApp_Noite/Controllers/ClienteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Noite.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
